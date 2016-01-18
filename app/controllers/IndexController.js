64fd2bc58a51dabecb0d0071c4c77726fe30c8c2
Thanh Binh <technobia@gmail.com>
2016-01-18 22:41:40 +0700

    jssor slider on home page

--- app/controllers/IndexController.js
+++ app/controllers/IndexController.js
@@ -12,7 +12,29 @@ define(function(require) {
     }
 
     IndexController.prototype.onLoad = function() {
-        console.log('Mother Fucker! Welcome to the first');
+        this.sliderLoaded();
+    };
+
+    IndexController.prototype.sliderLoaded = function() {
+        jQuery(document).ready(function ($) {
+            var options = { $AutoPlay: true };
+            var jssor_slider1  = new $JssorSlider$('sliderHome', options);
+
+            function ScaleSlider() {
+                var parentWidth = $('#sliderHome').parent().width();
+                if (parentWidth) {
+                    jssor_slider1.$ScaleWidth(parentWidth);
+                } else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+
+            ScaleSlider();
+
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+        });
     };
 
     return IndexController;
